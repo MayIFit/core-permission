3fc8d5cb503a1ea0aa3889737ef2e527f0e11c1f
Tischler Kristóf <tischler.kristof@gmail.com>
2020-03-26 23:56:00 +0100

    - nice try, but obsolute

--- src/Database/migrations/2020_03_18_175720_extend_users_table.php
+++ src/Database/migrations/2020_03_18_175720_extend_users_table.php
@@ -1,12 +1,10 @@
 <?php
 
-namespace MayIFit\Core\Permission\Database\Migrations;
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ExtendUsersTable extends Migration
+class PermissionExtendUsersTable extends Migration
 {
     /**
      * Run the migrations.
