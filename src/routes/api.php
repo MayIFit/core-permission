8c52bc00118be8c4cdcd508a3d54b128e3189851
Tischler Kristóf <tischler.kristof@gmail.com>
2020-03-27 00:16:36 +0100

    Remove duplicated prefix

--- src/routes/api.php
+++ src/routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['middleware' => ['api', 'auth:api'], 'prefix' => 'api/v1'], function () {
-    Route::namespace('MayIFit\Core\Permission\Http\Controllers\API')->prefix('admin/permission')->name('api.admin.')->group(function() { 
+    Route::namespace('MayIFit\Core\Permission\Http\Controllers\API')->prefix('admin')->name('api.admin.')->group(function() { 
         Route::apiResource('permission', 'PermissionController@', ['except' => ['store', 'show', 'update', 'destroy']]);
     });
 });
