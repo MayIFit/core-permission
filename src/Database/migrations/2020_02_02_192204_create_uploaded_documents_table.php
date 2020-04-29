bba296b74023db493bc8079fb264a3dbcd74264d
Tischler Kristóf <tischler.kristof@gmail.com>
2020-04-29 09:47:58 +0200

    Moved schemas from ext/shop

---
+++ src/Database/migrations/2020_02_02_192204_create_uploaded_documents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUploadedDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uploaded_documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('original_file_name');
+            $table->string('resource_url');
+            $table->string('description')->nullable();
+            $table->unsignedBigInteger('created_by')->references('id')->on('users')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uploaded_documents');
+    }
+}
---
+++ src/Database/migrations/2020_02_02_192443_create_entity_uploaded_document_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductUploadedDocumentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entity_uploaded_document', function (Blueprint $table) {
+            $table->unsignedBigInteger('document_id')->references('id')->on('uploaded_documents');
+            $table->unsignedBigInteger('entity_id')->references('id');
+            $table->primary(['product_photo_id', 'entity']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entity_uploaded_document');
+    }
+}
