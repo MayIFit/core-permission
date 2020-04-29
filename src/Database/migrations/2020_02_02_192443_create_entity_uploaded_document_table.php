64e7b59430ab85f0c5e388a14cd0a761852e2bc0
Tischler Kristóf <tischler.kristof@gmail.com>
2020-04-29 10:00:21 +0200

    Fixes

--- src/Database/migrations/2020_02_02_192443_create_entity_uploaded_document_table.php
+++ src/Database/migrations/2020_02_02_192443_create_entity_uploaded_document_table.php
@@ -15,7 +15,8 @@ class CreateEntityUploadedDocumentTable extends Migration
     {
         Schema::create('entity_uploaded_document', function (Blueprint $table) {
             $table->unsignedBigInteger('document_id')->references('id')->on('uploaded_documents');
-            $table->unsignedBigInteger('entity_id')->references('id');
+            $table->unsignedBigInteger('entity_id');
+            $table->string('entity_type');
             $table->primary(['product_photo_id', 'entity']);
         });
     }
