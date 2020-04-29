46472f97911fbb6043ef4476164283db17a4d106
Tischler Kristóf <tischler.kristof@gmail.com>
2020-04-29 14:27:07 +0200

    Split single and multi file upload

---
+++ src/GraphQL/Mutations/UploadMultiple.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\GraphQL\Mutations\Extensions;
+
+use Illuminate\Support\Facades\Storage;
+
+class UploadMultiple
+{
+
+    protected $pathMatrix = [
+        'product_photo' => 'product_images',
+        'product_file' => 'product_additional',
+        'user_avatar' => 'images'
+    ];
+
+    protected $classMatrix = [
+        'product_photo' => \MayIFit\Extension\Shop\Models\Document::class,
+        'product_file' => \MayIFit\Extension\Shop\Models\Document::class,
+        'user_avatar' => \MayIFit\Core\Permission\Models\Document::class
+    ];
+
+    /**
+     * Upload a file, store it on the server and return the path.
+     *
+     * @param  mixed  $root
+     * @param  mixed[]  $args
+     * @return string|null
+     */
+    public function resolve($root, array $args): ?string
+    {
+        /** @var \Illuminate\Http\UploadedFile $file */
+        $files = $args['input'];
+
+        foreach ($files as $element) {
+            $type = $element['type'];
+            $file = $element['file'];
+            
+            $path = $this->pathMatrix[$type] ?? '';
+    
+            if (!$path) {
+                return json(['error' => 'Don\'t know where to save file']);
+            }
+
+            $storeName = str_replace(' ', '_', $file->getClientOriginalName());
+
+            $storedPath = $file->storeAs($path, $storeName);
+            $document = new $this->classMatrix[$type];
+            $document->name = $path;
+            $document->resource_url = Storage::url($storedPath);
+            $document->original_file_name = $file->getClientOriginalName();
+            $document->save();
+    
+    
+            return $storedPath;
+        }
+    }
+}
