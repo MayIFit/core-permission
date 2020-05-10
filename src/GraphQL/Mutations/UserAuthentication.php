8b6851b2cd1f33d882b1f0511d321b48fdb0e981
Tischler Kristóf <tischler.kristof@gmail.com>
2020-05-10 23:37:45 +0200

    Moved to Mutations

--- src/GraphQL/Mutations/UserAuthentication.php
+++ src/GraphQL/Mutations/UserAuthentication.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\GraphQL\Queries\Core;
+namespace App\GraphQL\Mutations\Core;
 
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
