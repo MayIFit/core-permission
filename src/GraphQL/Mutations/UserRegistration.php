98e72eb8767c55876343ba2308a4fc5441d513b2
Tischler Kristóf <tischler.kristof@gmail.com>
2020-05-10 21:25:37 +0200

    Fixes

--- src/GraphQL/Mutations/UserRegistration.php
+++ src/GraphQL/Mutations/UserRegistration.php
@@ -6,6 +6,7 @@ use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 use MayIFit\Core\Permission\Exceptions\MisMatchedAuthorizationRequest;
 use App\Models\User;
