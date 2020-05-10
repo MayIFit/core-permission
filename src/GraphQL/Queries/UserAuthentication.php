11970be71ed086ce5106226a4283ce0bcd53be90
Tischler Kristóf <tischler.kristof@gmail.com>
2020-05-10 21:17:04 +0200

    Fixes

--- src/GraphQL/Queries/UserAuthentication.php
+++ src/GraphQL/Queries/UserAuthentication.php
@@ -18,8 +18,8 @@ class UserAuthentication
      * @return void
      */
     public static function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) {
-        $email = $args['email'];
-        $password = $args['password'];
+        $email = $args['input']['email'];
+        $password = $args['input']['password'];
 
         $user = User::where('email', $email)->first();
     
