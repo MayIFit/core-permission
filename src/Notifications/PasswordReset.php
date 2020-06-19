b4964759818095f218f259567d120494deffa1a1
Tischler Kristóf <tischler.kristof@gmail.com>
2020-06-19 11:23:44 +0200

    Fixes

--- src/Notifications/PasswordReset.php
+++ src/Notifications/PasswordReset.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MayIFit\Extension\Shop\Notifications;
+namespace MayIFit\Core\Permission\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
