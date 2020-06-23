b30a8a6a70c4dd9e516b728141ab64f299485843
Tischler Kristóf <tischler.kristof@gmail.com>
2020-06-23 10:03:46 +0200

    Refactor system settings handling

--- src/GraphQL/Queries/PublicSystemSettings.php
+++ src/GraphQL/Queries/PublicSystemSettings.php
@@ -4,12 +4,12 @@ namespace App\GraphQL\Queries\Core;
 
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
-use Illuminate\Support\Facades\DB;
 
-class AllSystemSettings
+use MayIFit\Core\Permission\Models\SystemSetting;
+
+class PublicSystemSettings
 {
     public function __invoke($rootValue,array $args, GraphQLContext $context, ResolveInfo $resolveInfo) {
-        $systemSettings = DB::table('system_settings')->get();
-        return $systemSettings ?? [];
+        return SystemSetting::where('public', true)->get();
     }
 }
